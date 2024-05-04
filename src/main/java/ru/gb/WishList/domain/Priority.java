2effde4dd18c9174eaba8915569a284a77cc0fea
GlazkovaMarina <glazkova.mv@bk.ru>
2024-05-04 13:51:17 +0300

    add enum Priority

---
+++ src/main/java/ru/gb/WishList/domain/Priority.java
@@ -0,0 +1,13 @@
+package ru.gb.WishList.domain;
+
+/**
+ * Priority - приоритет подарка для желателя
+ * HIGH - высокий (хотелось бы получить в подарок в первую очередь)
+ * MIDDLE - средний
+ * LOW - низкий
+ **/
+public enum Priority {
+    HIGH,
+    MIDDLE,
+    LOW
+}
